dabd6bb513bfc0b3f5587041ac2372df79873119
Neha <Neha@localhost>
2021-01-15 14:02:02 -0800

    LargestElement

--- src/main/java/com/practive/coding/LargestElement.java
+++ src/main/java/com/practive/coding/LargestElement.java
@@ -5,15 +5,13 @@ import java.util.Arrays;
 public class LargestElement {
 	public static void main(String args[]) {
 		int[] sampleArray = { 12, 22, 3, 4, 5, 9, 10, 11, 123, 125, 179 };
-		System.out.println(findLargest(sampleArray, 5));
+		System.out.println(findLargest(sampleArray, 1));
 	}
 
 	private static int findLargest(int[] nums, int k) {
 		Arrays.sort(nums);
-		for (int i : nums) {
-			System.out.print(i + " ");
-		}
-		return nums[nums.length - k];
+		int n = nums.length;
+		return nums[n - k];
 	}
 
 }
